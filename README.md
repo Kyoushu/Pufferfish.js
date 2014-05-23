b9d6b2c1b54fc5acca70fd1ea4b2283d29c42a4b
Kyoushu <kyoushu:gmail.com>
2014-05-23 17:13:32 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-Pufferfish.js
-=============
+# Pufferfish.js
 
-Container size responsive image loader
+Loads images based on the amount of space available in the image's containing element.
+
+This is still very much a proof of concept, so don't use it for anything important.
+
+## Usage Example
+
+    <style>
+        .container{
+            width: 50%;
+            margin: 0 auto 0 auto;
+        }
+
+        img{
+            width: 100%;
+        }
+    </style>
+
+    <div class="container">
+        <img data-pufferfish="[http://placehold.it/640x480, (max-width: 640)], [http://placehold.it/1200x500, (min-width: 641)]" />
+    </div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/pufferfish.js"></script>
+
+    <script>$(document).pufferfish()</script>
---
+++ pufferfish.js
@@ -0,0 +1,133 @@
+(function($, window, document, undefined){
+    
+    $.fn.pufferfish = function(){
+        
+        var namespace = 'pufferfish';
+        
+        var sizeDefinitionRegex =       /\[([^,]+)(\s+)?,(\s+)?\(([^\)]+)\)(\s+)?\]/;
+        var sizeDefinitionRegexGlobal = /\[([^,]+)(\s+)?,(\s+)?\(([^\)]+)\)(\s+)?\]/g;
+        var constraintRegex =       /([^:]+):(\s+)?([^,]+)/;
+        var constraintRegexGlobal = /([^:]+):(\s+)?([^,]+)/g;
+        
+        var watchedImages = [];
+        
+        function getDataAttrKey(name){
+            return 'data-' + namespace + (typeof name !== 'undefined' ? '-' + name : '');
+        }
+        
+        function getDataAttrSelector(name){
+            return '[' + getDataAttrKey(name) + ']';
+        }
+        
+        function parseConstraints(constraintString){
+            
+            var defaults = {
+                'max-width': null,
+                'min-width': null
+            };
+            
+            var constraints = {};
+            
+            var constraintStringParts = constraintString.match(constraintRegexGlobal);
+            constraintStringParts.forEach(function(constraintStringPart){
+                var constraintVars = constraintStringPart.match(constraintRegex);
+                constraints[constraintVars[1]] = parseInt(constraintVars[3]);
+            });
+            
+            return $.extend({}, defaults, constraints);
+            
+        }
+        
+        function parseSizeDefinitions(sizeDefString){
+            
+            var sizeDefStringParts = sizeDefString.match(sizeDefinitionRegexGlobal);
+            var sizeDefs = [];
+            
+            sizeDefStringParts.forEach(function(sizeDefStringPart){
+                var sizeDefVars = sizeDefStringPart.match(sizeDefinitionRegex);
+                
+                sizeDefs.push({
+                    'src': sizeDefVars[1],
+                    'constraints': parseConstraints(sizeDefVars[4])
+                });
+                
+            });
+            
+            return sizeDefs;
+            
+        }
+        
+        function watchImage(img){
+            
+            var sizeDefString = img.attr( getDataAttrKey() );
+            var sizeDefinitions = parseSizeDefinitions(sizeDefString);
+            
+            watchedImages.push({
+                'img': img,
+                'sizeDefinitions': sizeDefinitions
+            });
+        
+        }
+        
+        var delayedReflowTimeout;
+        var delayedReflowDelay = 100;
+        
+        function delayedReflow(){
+            clearTimeout(delayedReflowTimeout);
+            delayedReflowTimeout = setTimeout(function(){
+                reflow();
+            }, delayedReflowDelay);
+        }
+        
+        function reflow(){
+            
+            watchedImages.forEach(function(watchedImage){
+                
+                var img = watchedImage.img;
+                var sizeDefinitions = watchedImage.sizeDefinitions;
+                var container = img.parent();
+                var availableWidth = container.innerWidth();
+                
+                var selectedSizeDefinition = null;
+                
+                $.each(sizeDefinitions, function(key, sizeDefinition){
+                   
+                    if(
+                        sizeDefinition.constraints['min-width'] !== null &&
+                        availableWidth < sizeDefinition.constraints['min-width']
+                    ){
+                        return;
+                    }
+                    
+                    if(
+                        sizeDefinition.constraints['max-width'] !== null &&
+                        availableWidth >= sizeDefinition.constraints['max-width']
+                    ){
+                        return;
+                    }
+                    
+                    selectedSizeDefinition = sizeDefinition;
+                    return false;
+                    
+                });
+                
+                if(selectedSizeDefinition !== null){
+                    img.attr('src', selectedSizeDefinition.src);
+                }
+            
+            });
+            
+        }
+        
+        $(this).find( 'img' + getDataAttrSelector() ).each(function(){
+            var img = $(this);
+            watchImage(img);
+        });
+        
+        $(window).on('resize', reflow);
+        
+        reflow();
+        
+    };
+    
+})(jQuery, window, window.document);
